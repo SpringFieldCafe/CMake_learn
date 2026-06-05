4c6975239608a8e8f1707d57125a1d7f39567e60
綾波あやなみ <2574703489@qq.com>
2026-06-05 21:55:38 +0800

    use cmake and auto ,std=c11

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/25747/Desktop/cmake_learn/add.cpp" "CMakeFiles/app.dir/add.cpp.obj" "gcc" "CMakeFiles/app.dir/add.cpp.obj.d"
+  "C:/Users/25747/Desktop/cmake_learn/dif.cpp" "CMakeFiles/app.dir/dif.cpp.obj" "gcc" "CMakeFiles/app.dir/dif.cpp.obj.d"
+  "C:/Users/25747/Desktop/cmake_learn/div.cpp" "CMakeFiles/app.dir/div.cpp.obj" "gcc" "CMakeFiles/app.dir/div.cpp.obj.d"
+  "C:/Users/25747/Desktop/cmake_learn/main.cpp" "CMakeFiles/app.dir/main.cpp.obj" "gcc" "CMakeFiles/app.dir/main.cpp.obj.d"
+  "C:/Users/25747/Desktop/cmake_learn/mul.cpp" "CMakeFiles/app.dir/mul.cpp.obj" "gcc" "CMakeFiles/app.dir/mul.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "C:/Users/25747/Desktop/cmake_learn/output_use_set/t12/t2/app.exe"
+  "C:/Users/25747/Desktop/cmake_learn/output_use_set/t12/t2/app.exe.manifest"
+  "C:/Users/25747/Desktop/cmake_learn/output_use_set/t12/t2/app.pdb"
+  "C:/Users/25747/Desktop/cmake_learn/output_use_set/t12/t2/libapp.dll.a"
+  "CMakeFiles/app.dir/add.cpp.obj"
+  "CMakeFiles/app.dir/add.cpp.obj.d"
+  "CMakeFiles/app.dir/dif.cpp.obj"
+  "CMakeFiles/app.dir/dif.cpp.obj.d"
+  "CMakeFiles/app.dir/div.cpp.obj"
+  "CMakeFiles/app.dir/div.cpp.obj.d"
+  "CMakeFiles/app.dir/main.cpp.obj"
+  "CMakeFiles/app.dir/main.cpp.obj.d"
+  "CMakeFiles/app.dir/mul.cpp.obj"
+  "CMakeFiles/app.dir/mul.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
