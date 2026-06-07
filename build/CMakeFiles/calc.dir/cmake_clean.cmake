bb817db54e0da48853e80f9a72366434e6c2fda2
綾波あやなみ <2574703489@qq.com>
2026-06-07 18:31:20 +0800

    build shared lib without pointing out the output path

---
+++ build/CMakeFiles/calc.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc.dir/src/add.cpp.obj"
+  "CMakeFiles/calc.dir/src/add.cpp.obj.d"
+  "CMakeFiles/calc.dir/src/dif.cpp.obj"
+  "CMakeFiles/calc.dir/src/dif.cpp.obj.d"
+  "CMakeFiles/calc.dir/src/div.cpp.obj"
+  "CMakeFiles/calc.dir/src/div.cpp.obj.d"
+  "CMakeFiles/calc.dir/src/mul.cpp.obj"
+  "CMakeFiles/calc.dir/src/mul.cpp.obj.d"
+  "libcalc.dll"
+  "libcalc.dll.a"
+  "libcalc.dll.manifest"
+  "libcalc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
