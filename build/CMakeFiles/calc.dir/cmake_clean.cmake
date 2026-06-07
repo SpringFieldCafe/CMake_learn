472d5be3c3aa58bbfb6050c14d112b7d217fe2cc
綾波あやなみ <2574703489@qq.com>
2026-06-07 18:46:09 +0800

    add path

--- build/CMakeFiles/calc.dir/cmake_clean.cmake
+++ build/CMakeFiles/calc.dir/cmake_clean.cmake
@@ -1,4 +1,8 @@
 file(REMOVE_RECURSE
+  "C:/Users/25747/Desktop/cmake_learn/output_Shared/lib/libcalc.dll"
+  "C:/Users/25747/Desktop/cmake_learn/output_Shared/lib/libcalc.dll.a"
+  "C:/Users/25747/Desktop/cmake_learn/output_Shared/lib/libcalc.dll.manifest"
+  "C:/Users/25747/Desktop/cmake_learn/output_Shared/lib/libcalc.pdb"
   "CMakeFiles/calc.dir/src/add.cpp.obj"
   "CMakeFiles/calc.dir/src/add.cpp.obj.d"
   "CMakeFiles/calc.dir/src/dif.cpp.obj"
@@ -7,10 +11,6 @@ file(REMOVE_RECURSE
   "CMakeFiles/calc.dir/src/div.cpp.obj.d"
   "CMakeFiles/calc.dir/src/mul.cpp.obj"
   "CMakeFiles/calc.dir/src/mul.cpp.obj.d"
-  "libcalc.dll"
-  "libcalc.dll.a"
-  "libcalc.dll.manifest"
-  "libcalc.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
